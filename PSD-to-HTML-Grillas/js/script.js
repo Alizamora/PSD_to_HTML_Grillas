2259fd1c951569705f3201a91645c91ec3859db2
Alisson Zamora <alison.zam18@gmail.com>
2018-11-13 22:52:36 -0600

    Slider casi listo

--- PSD-to-HTML-Grillas/js/script.js
+++ PSD-to-HTML-Grillas/js/script.js
@@ -5,7 +5,7 @@ let contador = 0;
 let slider = document.getElementById('sliders');
 
 window.setInterval(function(){
-contador ++;
+contador++;
 if (contador > 2) contador = 0;
 slider.style.setProperty('left', contador * -100 + 'vw');
 }, 5000);
