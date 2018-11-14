afce26c2d7ebdcd261976ae05960cb09c29bffb8
Alisson Zamora <alison.zam18@gmail.com>
2018-11-13 21:19:38 -0600

    Analisis y menu terminado, slider en proceso

---
+++ PSD-to-HTML-Grillas/js/script.js
@@ -0,0 +1,13 @@
+
+/*SLIDER*/ 
+
+let contador = 0;
+let slider = document.getElementById('sliders');
+
+window.setInterval(function(){
+contador ++;
+if (contador > 2) contador = 0;
+slider.style.setProperty('left', contador * -100 + 'vw');
+}, 5000);
+
+/*END SLIDER */
